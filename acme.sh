bac4e973e77a281ab2586911f69f1080f6a9cf2e
baixiaojian <xiaojian.bai@advance.ai>
2021-10-09 19:23:00 +0800

    ci: update

--- acme.sh
+++ acme.sh
@@ -1,9 +1,9 @@
 { # try
 
-    echo '111' && echo '222' && git add .
+    echo '111' && echo '222' && git push
     #save your output
 
 } || { # catch
-echo "aaa"
+    echo "aaa"
     # save log for exception
 }
